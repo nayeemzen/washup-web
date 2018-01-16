6a9857fbc2a0cc2e8810e215a7fcd07d6f6cf308
Nayeem Zen <nayeem.zen@mail.utoronto.ca>
2018-01-15 22:21:40 -0500

    Make TabSelector stateful

--- src/components/common/TabSelector.js
+++ src/components/common/TabSelector.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './TabSelector.css';
 
-const TabSelector = ({options}) => (
-  <div className="TabSelector">
+class TabSelector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: props.options
+    }
+  }
+
+  render() {
+    return <div className="TabSelector">
       {
-        options.map((option, idx) =>
+        this.state.options.map((option, idx) =>
           <li
             key={idx}
+            onClick={() => this.selectOption(idx)}
             className={option.enabled ? "enabled" : "disabled"}>
             {option.name}
           </li>)
       }
-  </div>
-);
+    </div>
+  }
+
+  selectOption = (selectedIdx) => {
+    this.setState({
+      options: this.state.options.map((option, idx) => {
+        if (selectedIdx === idx) {
+          return {
+            name: option.name,
+            enabled: true
+          }
+        } else {
+          return {
+            name: option.name,
+            enabled: false
+          }
+        }
+      })
+    })
+  }
+}
 
 export default TabSelector;
