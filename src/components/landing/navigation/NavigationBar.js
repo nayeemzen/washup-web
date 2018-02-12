9ad42f3399cf3a7a24941b545379763b0f5f6c71
Nayeem Zen <nayeem.zen@mail.utoronto.ca>
2018-02-12 04:25:19 -0500

    Add mobile menu

--- src/components/landing/navigation/NavigationBar.js
+++ src/components/landing/navigation/NavigationBar.js
@@ -1,18 +1,68 @@
 import React from 'react';
+import Modal from 'react-modal';
 import Logo from '../../../resources/logo_horizontal_selected.svg';
 import './NavigationBar.css';
+import modalStyles from "../../order/ModalStyles";
 
 class NavigationBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      modalOpen: false
+    }
+  }
+  componentWillMount() {
+    Modal.setAppElement('#root')
+  }
+
+  toggleModal = () => {
+    this.setState((prevState) => {
+      return {
+        modalOpen: !prevState.modalOpen
+      }
+    });
+  };
+
   render() {
+    const {children} = this.props;
     return (
       <div className="NavigationBar">
         <div className="Logo">
           <object data={Logo}>Wash Up</object>
         </div>
-        <div className="Links">
-          {this.props.children}
-        </div>
-        <i className="fa fa-bars" aria-hidden="true"/>
+        <div className="Links">{children}</div>
+        <i
+          className="fa fa-bars"
+          aria-hidden="true"
+          onClick={this.toggleModal}
+        />
+        <Modal
+          isOpen={this.state.modalOpen}
+          style={{
+            overlay : {
+              zIndex: 10,
+              backgroundColor: 'rgba(41, 153, 187, 1)',
+            },
+            content: {
+              backgroundColor: 'transparent',
+              border: 'none',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: "center",
+              justifyContent: "flex-start"
+            }
+          }}>
+            <div className="MobileMenu">
+              <span
+                className="close"
+                onClick={this.toggleModal}>
+                ⨉
+              </span>
+              <div className="MobileLinks">
+                {children}
+              </div>
+            </div>
+        </Modal>
       </div>
     );
   }
