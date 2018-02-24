38fdee5fbe9470bf7a16e003cc8f84b8b3118a31
Ali Hussain <alihussaincs@gmail.com>
2018-02-24 14:09:51 -0500

    WashUp Headers css

--- src/components/landing/header/Header.js
+++ src/components/landing/header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 
 const Header = ({headerText}) => (
-  <section className="Header">
+  <section className="LandingHeader">
     <div><h1>{headerText}</h1></div>
   </section>
 );
